407c2164765e74485de025ed6fb00b2310d36e02
SHIVANI S <108917159+jeonshivh@users.noreply.github.com>
2023-08-25 03:33:37 -0700

    Add files via upload

---
+++ Day_6/src/main/java/com/example/demo/repository/Six_Rep_intf.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Entity_cls;
+
+public interface Six_Rep_intf extends JpaRepository<Entity_cls, Integer>{
+  
+}
