2492d51908f0ad4797e818e11ca653d793ff38bc
SHIVANI S <108917159+jeonshivh@users.noreply.github.com>
2023-08-28 11:46:32 +0530

    Delete Day_6 directory

--- Day_6/src/main/java/com/example/demo/repository/Six_Rep_intf.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Entity_cls;
-
-public interface Six_Rep_intf extends JpaRepository<Entity_cls, Integer>{
-  
-}
