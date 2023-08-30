6f754b91db9f20e41ed099ffe6657062445daa7e
SHIVANI S <108917159+jeonshivh@users.noreply.github.com>
2023-08-30 09:32:19 +0530

    Delete Glam_Art_app directory

--- Glam_Art_app/src/main/java/com/example/demo/repository/Sel_intf.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Sel_pojo;
-
-public interface Sel_intf extends JpaRepository<Sel_pojo, Integer>{
-
-}
--- Glam_Art_app/src/main/java/com/example/demo/repository/User_intf.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.User_pojo;
-
-public interface User_intf extends JpaRepository<User_pojo, Integer>{
-
-}
